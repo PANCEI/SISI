99a4d5eb24b37ac1744b329129698756c114336d
PANCEI <aswarhamid12@gmail.com>
2023-08-04 11:10:18 +0800

    menyelesaikan login

--- sisi/app/Controllers/Login.php
+++ sisi/app/Controllers/Login.php
@@ -3,11 +3,59 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
 
 class Login extends BaseController
 {
+    protected $userModel;
+    public function __construct()
+    {
+        $this->userModel = new UserModel();
+    }
     public function index()
     {
         return view("login/login");
+    }
+    public function cek()
+    {
+        $rules = [
+            'username'   => ['rules' => 'required', 'errors' => ['required' => 'Username is required']],
+            'password'   => ['rules' => 'required', 'errors' => ['required' => 'Password is required']],
+        ];
+
+        if (!$this->validate($rules)) {
+            session()->setFlashdata('error', 'Login gagal');
+            return redirect()->to('/login')->withInput();
+        }
+        // ambil variablenya ,
+        $username = $this->request->getVar('username');
+        $password = $this->request->getVar('password');
+
+        // ambil funct9ion get user name
+        $user = $this->userModel->GetUserName($username);
+        // cek jika user ada 
+        if ($user) {
+            // periksa apakah user active atau tidak 
+            if ($user['STATUS_USER'] == 'active') {
+                // cek password verify 
+                if (password_verify($password, $user['PASSWORD'])) {
+                    // buat session yang akan digunakan untuk nanti 
+                    $data = [
+                        'id_user' => $user['ID_USER'],
+                    ];
+                    session()->set($data);
+                    return redirect()->to('/Dashboard');
+                } else {
+                    session()->setFlashdata('error', 'Maaf Password Anda Masukan Salah');
+                    return  redirect()->to('/login')->withInput();
+                }
+            } else {
+                session()->setFlashdata('error', 'Akun ANda Tidak Aktive silahakan Hubungi Admin Yang bersangkutan');
+                return redirect()->to('Login')->withInput();
+            }
+        } else {
+            session()->setFlashdata('error', 'Maaf Anda TidakTerdaftar Dalam Sistem Mohon menghubungi admin yang bersangkutan');
+            return redirect()->to('/login')->withInput();
+        }
     }
 }
---
+++ sisi/app/Models/UserModel.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model
+{
+
+    protected $table            = 'user';
+    protected $primaryKey       = 'ID_USER';
+    protected $allowedFields    = [];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'CREATE_DATE';
+    protected $updatedField  = 'UPDATE_DATE';
+    protected $table2        =  'user_foto';
+    protected $on      = 'user_foto.ID_USER=user.ID_USER';
+    public function GetUserName($name)
+    {
+        return $this->where(['USERNAME' => $name])
+            ->join($this->table2, $this->on)
+            ->first();
+    }
+}
